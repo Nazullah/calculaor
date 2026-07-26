0e01dafdc1309a201d9fac41ca642381cf781b80
Nazullah <khang115khan@gmail.com>
2026-07-26 23:00:16 +0500

    change theme setting

--- calcu.js
+++ calcu.js
@@ -46,52 +46,52 @@ function calculate() {
     }
 }// Button Ripple Effect
 
-document.querySelectorAll("button").forEach(button=>{
+document.querySelectorAll("button").forEach(button => {
 
-button.addEventListener("click",function(e){
+    button.addEventListener("click", function (e) {
 
-const circle=document.createElement("span");
+        const circle = document.createElement("span");
 
-const size=Math.max(this.clientWidth,this.clientHeight);
+        const size = Math.max(this.clientWidth, this.clientHeight);
 
-circle.style.width=size+"px";
-circle.style.height=size+"px";
+        circle.style.width = size + "px";
+        circle.style.height = size + "px";
 
-const rect=this.getBoundingClientRect();
+        const rect = this.getBoundingClientRect();
 
-circle.style.left=e.clientX-rect.left-size/2+"px";
-circle.style.top=e.clientY-rect.top-size/2+"px";
+        circle.style.left = e.clientX - rect.left - size / 2 + "px";
+        circle.style.top = e.clientY - rect.top - size / 2 + "px";
 
-this.appendChild(circle);
+        this.appendChild(circle);
 
-setTimeout(()=>{
-circle.remove();
-},600);
+        setTimeout(() => {
+            circle.remove();
+        }, 600);
+
+    });
 
 });
-
-});
-let Theme=localStorage.getItem('theme') || 'calcu.css';
-function chengTheme(){
+let Theme = localStorage.getItem('theme') || 'calcu.css';
+function chengTheme() {
     let link = document.querySelector('link');
-    
+
     // localStorage.setItem('theme',Theme);
-    if(Theme==='calcu.css'){
+    if (Theme === 'calcu.css') {
         // Light Theme
-        link.setAttribute('href','calcu2.css');
-        Theme='calcu2.css';
-        localStorage.setItem('theme',Theme);
+        link.setAttribute('href', 'calcu2.css');
+        Theme = 'calcu2.css';
+        localStorage.setItem('theme', Theme);
         console.log('first block run');
     } else {
 
         // Dark Theme
-        link.setAttribute('href','calcu.css');
-        Theme='calcu.css';
-        localStorage.setItem('theme',Theme);
+        link.setAttribute('href', 'calcu.css');
+        Theme = 'calcu.css';
+        localStorage.setItem('theme', Theme);
         console.log('2nd block run');
 
     }
-console.log(Theme);
+    console.log(Theme);
 }
-let link=document.querySelector('link');
-link.setAttribute('href',Theme);
+let link = document.querySelector('link');
+link.setAttribute('href', Theme);
